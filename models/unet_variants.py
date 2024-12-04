64a761a7cb1dc038e574ebf30acbba66344c7915
Ndphuong-17 <dps17reds@gmail.com>
2024-12-04 20:12:35 +0700

    simple 2_3

--- models/unet_variants.py
+++ models/unet_variants.py
@@ -5,6 +5,7 @@ import math
 from .components import MLP, TransAoA, ReUnet3PlusDownBlock, ReUnet3PlusDownBlock_Smaller
 from .ReUNet2Plus1 import ReUNet2Plus1 as ReUNet2Plus
 from .SimpleReUNet2Plus1 import SimpleReUNet2Plus
+from .SimpleReUNet2Plus2 import SimpleReUNet2Plus as SimpleReUNet2Plus1
 
 class ReUNet(nn.Module):
   def __init__(self, noise_dim = 4, num_layers = 1, hidden_size = 256, filters = [16, 64, 128, 256], mid = True):
